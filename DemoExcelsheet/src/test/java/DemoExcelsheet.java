be7e5273dafd51b7090318a0f15fe05e5fc644ed
AsawariMane <147968872+AsawariMane@users.noreply.github.com>
2025-05-07 15:55:25 +0530

    Update DemoExcelsheet.java

--- DemoExcelsheet/src/test/java/DemoExcelsheet.java
+++ DemoExcelsheet/src/test/java/DemoExcelsheet.java
@@ -5,6 +5,9 @@ public class DemoExcelsheet {
 	public void m1() {
 		System.out.println("This is m1()");
 	}
+	public void m2() {
+		System.out.println("This is m2()");
+	}
 	public static void main(String[] args) {
 
 		System.out.println("This is Demo class");
